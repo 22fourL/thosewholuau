d65a0482164b1a3d6e4d7c1e0951a868f6a576f1
22fourL <v6jhvl6w32@gmail.com>
2025-11-19 19:06:16 -0800

    fixed bug

--- hideWatermark.lua
+++ hideWatermark.lua
@@ -4,12 +4,19 @@ local localplayer = game:GetService("Players").LocalPlayer
 
 local playerGui = localplayer.PlayerGui
 
-local currentGUI = playerGui:FindFirstChild("MainUI"):FindFirstChild("Watermark")
+local dest = playerGui:FindFirstChild("MainUI")
+local currentGUI = dest:FindFirstChild("Watermark")
 
 game:GetService("StarterGui"):SetCore("SendNotification",{
-	Title = "Made by 22Four_L",
-	Text = "This was made in 2 seconds lol",
-	Icon = nil
+	Title = "Made by 22Four_L", -- Required
+	Text = "This was made in 2 seconds lol", -- Required
+	Icon = nil -- Optional
 })
 
 currentGUI:Destroy()
+
+-- this way the game doesnt freak out
+local replacee = Instance.new("ImageLabel")
+replacee.Parent = dest
+replacee.Name = "Watermark"
+replacee.Position = UDim2.new(5, 0, 0, 0)
