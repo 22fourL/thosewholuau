52476f1260fcc88f3bf247857a24eff6fd4b4370
22fourL <v6jhvl6w32@gmail.com>
2025-12-26 19:44:41 -0800

    Create Utils.lua

---
+++ modules/Utils.lua
@@ -0,0 +1,21 @@
+-- returns nearest hrp and distance
+local function getNearestHumanoidRootPart(fromPosition)
+    local nearestHRP = nil
+    local shortestDistance = math.huge
+
+    for _, model in ipairs(workspace:GetChildren()) do
+        local humanoid = model:FindFirstChildOfClass("Humanoid")
+        local hrp = model:FindFirstChild("HumanoidRootPart")
+
+        if humanoid and hrp then
+            local distance = (hrp.Position - fromPosition).Magnitude
+
+            if distance < shortestDistance then
+                shortestDistance = distance
+                nearestHRP = hrp
+            end
+        end
+    end
+
+    return nearestHRP, shortestDistance
+end
