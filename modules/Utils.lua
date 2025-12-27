fcfaea971efe4b234272071fdf5c5530f641a00c
22fourL <v6jhvl6w32@gmail.com>
2025-12-26 21:18:36 -0800

    MORE FUNCTIONSSSS

--- modules/Utils.lua
+++ modules/Utils.lua
@@ -25,4 +25,56 @@ function utils.getNearestHumanoidRootPartFromCharacter(fromCharacter: Model)
 	return nearestHRP
 end
 
+-- parent is optional, otherwise is global.
+function util.cloneAndPlay(sfx: Sound, optionalParent: Instance?)
+	local clone = sfx:Clone()
+	
+	if optionalParent then
+		clone.Parent = optionalParent
+	else
+		clone.Parent = workspace.Game.Temp
+	end
+	
+	clone:Play()
+	Debris:AddItem(clone, clone.TimeLength + 2)
+end
+
+-- returns whatever a basepart has hit, nil if none.
+function util.getPartThatTouched(objectPart: Part): Instance?
+	local objecttable = workspace:GetPartBoundsInBox(objectPart.CFrame, objectPart.Size)
+	local humsHit = {}
+
+	for i, v in objecttable do
+
+		if v:IsA("BasePart") and not humsHit[v.Parent.Name] and v.Anchored == false then
+
+			humsHit[v.Parent.Name] = true
+
+			-- ok cool we hit them now
+			return v
+		end
+
+	end
+
+	return nil
+end
+
+
+-- this only returns true if the part is a humanoidrootpart
+function util.isAchildOfACharacter(part: Part)
+	if part.Parent == nil then
+		return false
+	end
+
+	if part.Parent:FindFirstChildOfClass("Humanoid")
+		and part:IsA("BasePart")
+		and part.Name ~= "Handle"
+		and Players:GetPlayerFromCharacter(part.Parent) ~= nil
+		and part.Name == "HumanoidRootPart" then
+		return true
+	else
+		return false
+	end
+end
+
 return utils
